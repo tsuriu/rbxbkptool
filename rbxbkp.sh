05f14b8389f19fb93f81d5a9d928e36e8fbb148c
tsuriu <tulioromeror@gmail.com>
2019-08-23 13:09:08 -0200

    Update

--- rbxbkp.sh
+++ rbxbkp.sh
@@ -76,7 +76,6 @@ bkpdisc(){
 
     if [ $diffDate -le 7 -a ! -f "$NFS_DIR/$finalfile" ]
     then
-      echo "fuck this shit.. $flname"
   #    cp $BKP_DIR/$flname $NFS_DIR/$finalfile
       echo "$flname has been transfered" >> $LOG_FILE
     fi
@@ -97,6 +96,8 @@ housekeeper(){
     datenow=$(date +%s)
 
     diffDate=$(getDateDiff $datenow $timestr)
+
+    echo $diffDate
 
     if [ $diffDate -gt 7 ]
     then
